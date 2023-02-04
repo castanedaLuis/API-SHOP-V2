1454a07e83b21f8a2f9d7f77f2dd74cd92b3e38a
castanedaLuis <castanedaLuis@github.com>
2023-02-04 16:50:25 -0600

    Integración de node-postgres

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
+const orderRouter = require('./orders.router');
 
 function routerApi(app) {
   const router = express.Router();
@@ -9,6 +11,7 @@ function routerApi(app) {
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
+  router.use('/orders', orderRouter);
 }
 
 module.exports = routerApi;
---
+++ services/order.service.js
@@ -0,0 +1,30 @@
+const boom = require('@hapi/boom');
+
+class OrderService {
+
+  constructor(){
+  }
+  async create(data) {
+    return data;
+  }
+
+  async find() {
+    return [];
+  }
+
+  async findOne(id) {
+    return { id };
+  }
+
+  async update(id, changes) {
+    return {
+      id,
+      changes,
+    };
+  }
+
+  async delete(id) {
+    return { id };
+  }
+
+}
